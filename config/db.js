300b32b494eb5f40fddfd6574fd37f48c2c6b2e4
Mariluz2024 <mluzco29@gmail.com>
2025-01-24 02:59:40 +0100

    Datos exportados

--- config/db.js
+++ config/db.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/luxury_cars', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Base de datos conectada');
-    } catch (error) {
-        console.error('Error al conectar la base de datos', error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(
+      "mongodb+srv://mluzco29:sHuGzApRDDPAPmKI@cluster0.gwnxg.mongodb.net/luxury_cars?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Base de datos conectada");
+  } catch (error) {
+    console.error("Error al conectar la base de datos", error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
